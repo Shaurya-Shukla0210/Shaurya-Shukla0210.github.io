c715dcc98dfb6ba88dc51d1b970523e14c5c166f
Shaurya-Shukla0210 <shauryashukla0210@gmail.com>
2025-12-31 09:18:42 +0530

    update

---
+++ mod4folder/script.js
@@ -0,0 +1,20 @@
+var names=new Array();
+names[0]="harsh";
+names[1]="Jason";
+names[2]="rapid";
+names[3]="jenny";
+names[4]="shivam";
+names[5]="jen";
+names[6]="pqr";
+names[7]="st";
+n
+
+
+for (var i = 0; i < names.length; i++) {
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+        console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("Hello "+ names[i])
+	}
+}
